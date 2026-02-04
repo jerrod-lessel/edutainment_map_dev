295b385b39dfe3533317a6a529b9f2be65fceee0
Jerrod Lessel <jerrod.lessel@gmail.com>
2026-02-04 12:33:06 -0800

    Update map.js

--- map.js
+++ map.js
@@ -1,1 +1,17 @@
+// Initialize the map
+const map = L.map('map', {
+  zoomControl: true,
+  attributionControl: false
+});
+
+// Guadalupe-focused view (rough center)
+map.setView([34.9715, -120.5713], 13);
+
+// Clean basemap (calm, modern)
+L.tileLayer(
+  'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+  {
+    maxZoom: 18
+  }
+).addTo(map);
 
